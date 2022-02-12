1be4e005f61deb8eed5be632ef444ec0ebe28508
Hector <stanleychike@yahoo.com>
2022-02-12 02:40:21 +0100

    subscriber1 api

---
+++ subscriber1/subscriber1/src/main/java/com/example/subscriber1/Subscriber1Application.java
@@ -0,0 +1,13 @@
+package com.example.subscriber1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Subscriber1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Subscriber1Application.class, args);
+	}
+
+}
---
+++ subscriber1/subscriber1/src/test/java/com/example/subscriber1/Subscriber1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.subscriber1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Subscriber1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
